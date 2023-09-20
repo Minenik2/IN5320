611bc32ba696498f87900755777209d94ac31cf9
Minenik2 <39184444+Minenik2@users.noreply.github.com>
2023-09-21 01:07:43 +0200

    2 days left and im vibin

--- oblig 2/in5320-assignment2-master/src/Table.js
+++ oblig 2/in5320-assignment2-master/src/Table.js
@@ -6,8 +6,23 @@ function Table(props) {
     return <p>Loading...</p>;
   } else {
     // Write your code here:
+    const countriesComponent = []
+    for (const iterator of props.apiData.results) {
+      countriesComponent.push(<tr> <td>{iterator.Country}</td> 
+      <td>{iterator.Continent}</td>
+      <td>{iterator.Population}</td>
+      <td>{iterator.PopulationGrowth}</td></tr>)
+    }
+    const countryMap = props.apiData.results.map((liste) => <td> {liste.Country} </td>)
     return <table>
-
+      <tr>
+        <th>Country</th>
+        <th>Continet</th>
+        <th>Population</th>
+        <th>Population Growth</th>
+      </tr>
+      {countriesComponent}
+      
     </table>;
   }
 }
